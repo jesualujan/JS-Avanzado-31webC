6607c014177d77e4f8795a6fd45b1cc4c2f94cf7
jesualujan <lujan.oficial@gmail.com>
2024-02-29 20:13:03 -0600

    ✨ ejemplo de pila de ejecución

---
+++ 02Async-Promesas/index.js
@@ -0,0 +1,16 @@
+const tarea1 = () => {
+    console.log('Tarea 1 iniciada');
+    setTimeout(() => {
+      console.log('Tarea 1 completada 🚀');
+    }, 2000);
+  };
+  
+  const tarea2 = () => {
+    console.log('Tarea 2 iniciada' );
+    setTimeout(() => {
+      console.log('Tarea 2 completada 🚀');
+    }, 1000);
+  };
+  
+  tarea1();
+  tarea2();
