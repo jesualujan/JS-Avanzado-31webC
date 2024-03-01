e70b1eeef2d54add8e9b4d4e4cdb97d74395b126
jesualujan <lujan.oficial@gmail.com>
2024-02-29 21:30:40 -0600

    ✨ ejemeplo de un callback

---
+++ 02Async-Promesas/callback.js
@@ -0,0 +1,27 @@
+//* EJEMPLO DE UN CALLBACK
+
+// tenemos 2 funciones que devuelven un valor
+
+function soyCien () {
+    return 100
+}
+
+
+function soyDoscientos() {
+    return 200
+}
+ 
+function soyTrescientos () {
+    return 300
+}
+
+// tenemos una nueva función llamada sumaDosFunciones
+// recibe 2 parametros que son mis 2 funciones (callbacks)
+function sumaDosFunciones (functionOne, functionTwo, functionThree){
+      const suma = functionOne() + functionTwo () + functionThree ()
+      return suma // retorna un nuevo valor, en este caso suma
+      console.log(suma)
+} 
+
+// Invocamos a sumaDosFunciones y le pasamos como parametro 2 funciones 
+console.log(sumaDosFunciones(soyCien, soyDoscientos, soyTrescientos))
