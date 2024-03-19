0027b798434be4a369c30a386e423ff53e39819d
jesualujan <lujan.oficial@gmail.com>
2024-03-19 12:40:48 -0600

    🐛  typeof fixed bug

--- 05CRUD/crud.js
+++ 05CRUD/crud.js
@@ -9,49 +9,50 @@ import arregloJedis from './data/schema.js';
     INICIANDO EL CAMINO JEDI: 1
 */
 
-const setRole = (jedi, role) => {
-    const roles= {
-        5: "Maestro de la orden",
+const setRole = (jedi,role) => {
+    const roles = {
+        5: "Maestro de la Orden",
         4: "Maestro Jedi",
         3: "Caballero Jedi",
         2: "Padawan",
         1: "Iniciando el camino Jedi"
     }
-}
 
-const validRole = roles[role]
+    const validRole = roles[role]
 
-if(validRole) {
-    // si el role es válido, se asigna al jedi
-    return {
-        ...jedi,
-        role: validRole
+    if(validRole){
+        //si el role es válido, se asigna al jedi
+        return{
+            ...jedi,
+            role: validRole
+        }
+    }else{
+          // si el rol no es válido, se asigna "Desconocido" al jedi  
+          return {
+            ...jedi,
+            role: "Desconocido"
+          } 
+      }
     }
-}else {
-    // si el rol no es válido, se asigna "Desconocido" al jedi
-    return {
-        ...jedi,
-        role: "Desconocido"
-    }
-}
 
 const handleFormSubmit = (event) => {
     e.preventDefault() // evitar que se envíe el formulario automaticamente 
 
     // Almacenar la información del formulario en un objeto de javascript 
-    const character = {
-        name: form.name.value,
-        age: Number(form.age.value),
-        team: form.team.value
-    }
+  const character = {
+    name: form.name.value,
+    age: Number(form.age.value),
+    team: form.team.value
+}
 
     // validar que el nombre del jedi no está repetido
     if (validCharacter (character)){
         // si los datos del personaje son validos, se agrega al arreglo de jedis y se actualiza la tabla
-        arregloJedis.push(character)
-        updateJediList()
+        arregloJedis.push(setRole(character, 1))
+    updateJediList()
         // si lo queremos ver por consola
         console.log(arregloJedis)
+        console.dir(arregloJedis)
     }else {
         // si hay datos vacios, se muestra un alerta y se detiene la ejecución del evento
         alert("TIENES UN DATO VACIO, POR FAVOR VERIFICAR EL FORMULARIO")
@@ -62,47 +63,61 @@ const handleFormSubmit = (event) => {
 const handleChangeRole = () => {
     // solicitar el nombre
     // guardar el indice con un click en la fila del crud 
-    const index = prompt("INGRESA EL INDICE DEL JEDI: " , " ")
+    const index = prompt('INGRESE EL INDICE DEL JEDI:', '')
     // que pasa si me da el nombre
     const jedi = arregloJedis[index]
-    if (jedi){
+        if(jedi){
         // utilizamos un modal de bootstrap
-        const newRole = prompt("INGRESA EL NUEVO ROL: " , " ")
-        if(newRole >= 1 && newRole <= 5){
+        const newRole = prompt('Ingrese el nuevo rol:' , '')
+            if(newRole >= 1 && newRole <= 5){
             // si el nuevo rol es válido, se actualiza el rol del personaje en el arreglo de jedis
             arregloJedis[index] = setRole(jedi, Number(newRole))
-            updateJediList()
-            console.log(arregloJedis)
+                updateJediList()
+                console.log(arregloJedis)
+            }else{
+                alert('Rol invalido')
+            }
         }else{
-            alert('EL ROL NO ES VALIDO')
+            alert('Indice inválido')
         }
-    }else{
-        alert('INDICE NO VALIDO')
     }
-}
 
-const handleEditJedi = () => {
 
-    // utilizar formulario en una modal de bootstrap
-    const newName = prompt("INGRESA EL NUEVO NOMBRE: " , jedi.name)
-    const newAge = prompt("INGRESA LA NUEVA EDAD: " , jedi.age)
-    const newTeam = prompt("INGRESA EL NUEVO EQUIPO: " , jedi.team)
+const handleEditJedi = (event) => {
+      // En JavaScript, la expresión event.target.dataset.index se utiliza para acceder al valor del 
+        // atributo data-index de un elemento HTML que ha desencadenado un evento. 
+        const index = event.target.dataset.index
+        const jedi = arregloJedis[index]
 
-    if( newName && newAge && newTeam){
-        // si se proporcionan nuevos valores válidos, se actuliza el jedi en el arreglo de jedis
-        arregloJedis[index] = {
-            ...jedi,
-            name: newName,
-            age: Number(newAge),
-            team: newTeam
+        //UTILIZAR FORMULARIO EN UNA MODAL DE BOOTSTRAP
+        const newName = prompt ('Ingrese el nuevo nombre', jedi.name)
+        const newAge = prompt ('Ingrese la nueva edad:', jedi.age)
+        const newTeam = prompt ('Ingrese el nuevo equipo:', jedi.team)
+
+        if(newName && newAge && newTeam){
+            // si se proporcionan nuevos valores válidos, se actualiza el jedi en el arreglo de jedis
+            arregloJedis[index] = {
+               ...jedi,
+               name: newName,
+               age: Number(newAge),
+               team: newTeam
+            }
+            updateJediList()
         }
-        updateJediList()
     }
-}
 
-const handleDeleteJedi = () => {
+    const handleDeleteJedi = (event) => {
+        // En JavaScript, la expresión event.target.dataset.index se utiliza para acceder al valor del 
+        // atributo data-index de un elemento HTML que ha desencadenado un evento. 
+        const index = event.target.dataset.index;
+        arregloJedis.splice(index, 1);
+    
+        updateJediList();
+        console.log(arregloJedis);
+    };
 
-    arregloJedis.splice(index, 1)
-    updateJediList()
-    console.log(arregloJedis)
-}
+    const handleFilterJedis = () => {
+        // La función trim() en JavaScript se utiliza para eliminar los espacios en blanco al principio y al final 
+        // de una cadena de texto. Esta función es muy útil para limpiar y normalizar cadenas de texto.
+
+    }
