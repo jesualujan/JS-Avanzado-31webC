3173446e3464d49eb194165a9233e8dcfc28a622
jesualujan <lujan.oficial@gmail.com>
2024-03-19 20:24:50 -0600

    🐛 fixed bug

--- 05CRUD/crud.js
+++ 05CRUD/crud.js
@@ -1,4 +1,4 @@
-import arregloJedis from './data/schema.js';
+import arregloJedis from "./data/schema.js";
 
 /*  RANGOS  
 
@@ -36,20 +36,20 @@ const setRole = (jedi,role) => {
     }
 
 const handleFormSubmit = (event) => {
-    e.preventDefault() // evitar que se envíe el formulario automaticamente 
+    event.preventDefault() // evitar que se envíe el formulario automaticamente 
 
     // Almacenar la información del formulario en un objeto de javascript 
-  const character = {
-    name: form.name.value,
-    age: Number(form.age.value),
-    team: form.team.value
-}
+    const character = {
+        name: form.name.value,
+        age: Number(form.age.value),
+        team: form.team.value
+    }
 
     // validar que el nombre del jedi no está repetido
-    if (validCharacter (character)){
+    if(validCharacter (character)){
         // si los datos del personaje son validos, se agrega al arreglo de jedis y se actualiza la tabla
         arregloJedis.push(setRole(character, 1))
-    updateJediList()
+        updateJediList()
         // si lo queremos ver por consola
         console.log(arregloJedis)
         console.dir(arregloJedis)
@@ -89,22 +89,23 @@ const handleEditJedi = (event) => {
         const index = event.target.dataset.index
         const jedi = arregloJedis[index]
 
-        //UTILIZAR FORMULARIO EN UNA MODAL DE BOOTSTRAP
-        const newName = prompt ('Ingrese el nuevo nombre', jedi.name)
-        const newAge = prompt ('Ingrese la nueva edad:', jedi.age)
-        const newTeam = prompt ('Ingrese el nuevo equipo:', jedi.team)
-
-        if(newName && newAge && newTeam){
-            // si se proporcionan nuevos valores válidos, se actualiza el jedi en el arreglo de jedis
-            arregloJedis[index] = {
-               ...jedi,
-               name: newName,
-               age: Number(newAge),
-               team: newTeam
-            }
-            updateJediList()
-        }
-    }
+         //UTILIZAR FORMULARIO EN UNA MODAL DE BOOTSTRAP
+         const newName = prompt ('Ingrese el nuevo nombre', jedi.name)
+         const newAge = prompt ('Ingrese la nueva edad:', jedi.age)
+         const newTeam = prompt ('Ingrese el nuevo equipo:', jedi.team)
+ 
+ 
+         if(newName && newAge && newTeam){
+             // si se proporcionan nuevos valores válidos, se actualiza el jedi en el arreglo de jedis
+             arregloJedis[index] = {
+                ...jedi,
+                name: newName,
+                age: Number(newAge),
+                team: newTeam
+             }
+             updateJediList()
+         }
+     }
 
     const handleDeleteJedi = (event) => {
         // En JavaScript, la expresión event.target.dataset.index se utiliza para acceder al valor del 
@@ -114,10 +115,115 @@ const handleEditJedi = (event) => {
     
         updateJediList();
         console.log(arregloJedis);
-    };
+    }
+
+    const handleSortJedis = () => {
+       // Implementar el algoritmo de ordenamiento por selección aquí
+       selectionSort(arregloJedis);
+    
+       // Actualizar la lista de jedis en el DOM
+       updateJediList();
+    }
 
     const handleFilterJedis = () => {
+        const filterValue = jediNameFilter.value.toLowerCase().trim()
         // La función trim() en JavaScript se utiliza para eliminar los espacios en blanco al principio y al final 
         // de una cadena de texto. Esta función es muy útil para limpiar y normalizar cadenas de texto.
+        const filteredJedis = arregloJedis.filter((jedi) => 
+         jedi.name.toLowerCase().includes(filterValue))
+
+         //actualizar la lista de jedis filtrados en el dom
+         updateJediList(filteredJedis)
+    }
+
+    // INTERCAMBIAR LOS ELEMENTOS DE POSICIÓN EN EL ARREGLO
+    // [a,b] = [b,a]
+    // -> ejemplo de stackoverflow: https://stackoverflow.com/questions/16201656/how-to-swap-two-variables-in-javascript
+
+    const selectionSort = (array) => {
+        const length = array.length
+
+     // ALGORITMO DE ORDENAMIENTO POR SELECCIÓN 
+    for (let i = 0; i < length - 1; i++) {
+        let minIndex = i 
+    
+        for ( let j = i + 1 ; j < length; j++){
+            if(array[j].name < array[minIndex].name){
+                minIndex = j
+            }
+        }
+
+        if (minIndex !== i){
+            swap(array, i, minIndex)
+        }
+     }
 
     }
+
+    const swap = (array, index1, index2) => {
+        // INTERCAMBIAR ELEMENTOS DE POSICIÓN EN EL ARREGLO 
+        const temp = array[index1]
+        array [index1]  = array[index2]
+        array[index2] = temp //array[index1]
+    }
+
+    // FUNCIÓN QUE CREA EL CRUD (Create, Read, Upadte/Patch , Delete)
+    const updateJediList = (filteredJedis = arregloJedis) => {
+        const jediList = document.querySelector('#jediList')
+        // limpiar el contenido de la tabla
+        jediList.innerHTML = ''  //LIMPIAR EL CONTENIDO EXISTENTE 
+
+       filteredJedis.forEach((jedi, index )=>{
+        const row = `
+        <tr>
+        <td>${jedi.name}</td>
+        <td>${jedi.age}</td>
+        <td>${jedi.team}</td>
+        <td>${jedi.role}</td>
+        <td>
+        <button type="button" class="btn btn-outline-warning editBtn" data-index="${index}">Editar</button>
+        <button type="button" class="btn btn-outline-danger deleteBtn" data-index="${index}">Eliminar</button>
+        </td>
+        </tr>   
+        `
+        jediList.innerHTML += row
+     })
+    
+
+        // agregar eventos a los botones de editar y eliminar 
+     const editButton = document.querySelectorAll ('.editBtn')
+     const deleteButton = document.querySelectorAll ('.deleteBtn')
+
+     editButton.forEach((button) =>{
+        button.addEventListener('click', handleEditJedi)
+     })
+
+     deleteButton.forEach((button) =>{
+        button.addEventListener('click', handleDeleteJedi)
+     })
+    }
+
+    
+  //VALIDAR QUE NO HAY PROPIEDADES VACIAS  // VAMOS A RECIBIR UN TRUE O FALSE (BOOLEANO)
+  const validCharacter = ({ name = "", age = 0, team=""}) => name !== "" && age !== 0 && team !== ""
+  
+  //EJECUCION 
+  updateJediList()
+
+  // OBTENIENDO EL FORMULARIO DESDE EL DOM (DESDE EL HTML)  SELECCIONAR ELEMENTO 
+  const form = document.querySelector('#characterRegistry')
+  //EVENTO                                                  MANIPULAR ELEMENTO 
+  form.addEventListener('submit', handleFormSubmit)
+
+  // AGREGAR EVENTO AL BOTÓN DE CAMBIAR ROL    SELECCIONAR ELEMENTO 
+  const changeRoleBtn = document.querySelector('#changeRoleBtn')
+  //EVENTO                                                  MANIPULAR ELEMENTO 
+  changeRoleBtn.addEventListener('click', handleChangeRole)
+
+  //AGREGAR UN EVENTO AL BOTÓN DE ORDENAR
+  const sortButton = document.querySelector('#orderButton')
+  sortButton.addEventListener('click', handleSortJedis )
+
+ //AGREGAR EVENTO AL INPUT DE ENTRADA DEL FILTRADO POR NOMBRE 
+ const jediNameFilter = document.querySelector('#jediNameFilter')
+ jediNameFilter.addEventListener('click', handleFilterJedis)
